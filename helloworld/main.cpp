6452f7f78ccb6db5115c1099dd86b2ae249c7317
Van-Minh Le <lvminh2k@gmail.com>
2020-12-18 14:59:39 +0700

    add helloworld project

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
+    vector<int> a;
+    a.push_back(1);
+    a.push_back(2);
+    for (int i:a){
+        cout << i <<endl;
+    }
     cout << "Hello world" << endl;
-    return 0
+    return 0;
 }
